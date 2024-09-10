b8e2b486d5789113bd008cf82e7bff8c9a11bd91
Vinit Agrawal <vinit@hellotars.com>
2024-09-10 14:41:02 +0530

    turned off eslint for a few things

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,7 +2,7 @@ module.exports = {
   extends: 'next/core-web-vitals',
   rules: {
     'react/no-unescaped-entities': 'off',
-    '@typescript-eslint/no-explicit-any': 'warn',
+    '@typescript-eslint/no-explicit-any': 'off',
     'prefer-const': 'warn',
   },
 };
